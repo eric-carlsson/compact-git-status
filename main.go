0c0abcd963258478c2c04df5a0f00cb82e750579
Eric Carlsson <97894605+eric-carlsson@users.noreply.github.com>
2024-07-20 16:34:31 +0200

    Add missing cases

--- main.go
+++ main.go
@@ -4,25 +4,55 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"slices"
 	"strconv"
 	"strings"
-
-	"github.com/eric-carlsson/go-git-status/symbols"
 )
 
 type Status struct {
-	Branch               string
-	UpstreamBranch       string
-	UpstreamBranchAhead  int
-	UpstreamBranchBehind int
-	Modified             []string
-	Untracked            []string
-	Staged               []string
-	Conflicts            []string
-	NumStashed           int
+	Commit    string
+	Branch    string
+	Upstream  string
+	Ahead     int
+	Behind    int
+	Staged    int
+	Conflict  int
+	Modified  int
+	Untracked int
+	Stashed   int
+}
+
+type Symbols struct {
+	Prefix    string
+	Suffix    string
+	Sep       string
+	Local     string
+	Ahead     string
+	Behind    string
+	Staged    string
+	Conflict  string
+	Modified  string
+	Untracked string
+	Stashed   string
+	Clean     string
 }
 
 func main() {
+	symbols := &Symbols{
+		Prefix:    "[",
+		Suffix:    "]",
+		Sep:       "|",
+		Local:     "L",
+		Modified:  "✚ ",
+		Staged:    "● ",
+		Conflict:  "✖ ",
+		Untracked: "…",
+		Stashed:   "⚑ ",
+		Ahead:     "↑·",
+		Behind:    "↓·",
+		Clean:     "✔",
+	}
+
 	output, err := gitStatus()
 	if err != nil {
 		log.Fatal(err)
@@ -33,7 +63,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(buildOutput(*status))
+	fmt.Println(buildOutput(status, symbols))
 }
 
 func gitStatus() (string, error) {
@@ -55,11 +85,14 @@ func gitStatus() (string, error) {
 
 func parseStatus(output string) (*Status, error) {
 	status := &Status{}
+
 	for _, line := range strings.Split(output, "\n") {
 		s := strings.Split(line, " ")
 		switch s[0] {
 		case "#":
 			switch s[1] {
+			case "branch.oid":
+				status.Commit = s[2]
 			case "branch.head":
 				status.Branch = s[2]
 			case "stash":
@@ -67,86 +100,82 @@ func parseStatus(output string) (*Status, error) {
 				if err != nil {
 					return nil, fmt.Errorf("parse num stashed: %w", err)
 				}
-				status.NumStashed = numStashed
+				status.Stashed = numStashed
 			case "branch.upstream":
-				status.UpstreamBranch = s[2]
+				status.Upstream = s[2]
 			case "branch.ab":
 				ahead, err := strconv.Atoi(s[2][1:])
 				if err != nil {
 					return nil, fmt.Errorf("parse ahead: %w", err)
 				}
-				status.UpstreamBranchAhead = ahead
+				status.Ahead = ahead
 
 				behind, err := strconv.Atoi(s[3][1:])
 				if err != nil {
 					return nil, fmt.Errorf("parse behind: %w", err)
 				}
-				status.UpstreamBranchBehind = behind
+				status.Behind = behind
 			}
-		case "1":
-			if s[1][1] == 'M' {
-				status.Modified = append(status.Modified, s[8])
-			}
-			if s[1][0] == 'U' {
-				status.Conflicts = append(status.Conflicts, s[8])
+		case "1", "2":
+			if slices.Contains([]string{"DD", "AU", "UD", "UA", "DU", "AA", "UU"}, s[1]) {
+				status.Conflict++
+			} else if s[1][1] == 'M' {
+				status.Modified++
 			} else {
-				status.Staged = append(status.Staged, s[8])
+				status.Staged++
 			}
 		case "?":
-			status.Untracked = append(status.Untracked, s[1])
+			status.Untracked++
 		}
 	}
 
 	return status, nil
 }
 
-func buildOutput(status Status) string {
+func buildOutput(status *Status, symbols *Symbols) string {
 	var b strings.Builder
 	b.WriteString(symbols.Prefix)
-	b.WriteString(status.Branch)
 
-	if status.UpstreamBranch == "" {
-		b.WriteString(fmt.Sprintf(" {%s}", symbols.Local))
+	if status.Branch == "(detached)" {
+		b.WriteString(fmt.Sprintf(":%s", status.Commit[:7]))
 	} else {
-		b.WriteString(fmt.Sprintf(" {%s}", status.UpstreamBranch))
+		b.WriteString(status.Branch)
 	}
 
-	if status.UpstreamBranchAhead > 0 {
-		b.WriteString(fmt.Sprintf(" %s%d", symbols.Ahead, status.UpstreamBranchAhead))
+	if status.Upstream == "" {
+		b.WriteString(fmt.Sprintf(" %s", symbols.Local))
+	} else {
+		b.WriteString(fmt.Sprintf(" {%s}", status.Upstream))
 	}
 
-	if status.UpstreamBranchBehind > 0 {
-		if status.UpstreamBranchAhead == 0 {
-			b.WriteString(" ")
+	if status.Ahead > 0 || status.Behind > 0 {
+		b.WriteString(" ")
+
+		if status.Ahead > 0 {
+			b.WriteString(fmt.Sprintf("%s%d", symbols.Ahead, status.Ahead))
 		}
 
-		b.WriteString(fmt.Sprintf("%s%d", symbols.Behind, status.UpstreamBranchBehind))
+		if status.Behind > 0 {
+			b.WriteString(fmt.Sprintf("%s%d", symbols.Behind, status.Behind))
+		}
 	}
 
 	b.WriteString(symbols.Sep)
 
-	if len(status.Staged) > 0 {
-		b.WriteString(fmt.Sprintf("%s%d", symbols.Staged, len(status.Staged)))
+	if status.Staged > 0 {
+		b.WriteString(fmt.Sprintf("%s%d", symbols.Staged, status.Staged))
 	}
-
-	if len(status.Conflicts) > 0 {
-		b.WriteString(fmt.Sprintf("%s%d", symbols.Conflict, len(status.Conflicts)))
+	if status.Conflict > 0 {
+		b.WriteString(fmt.Sprintf("%s%d", symbols.Conflict, status.Conflict))
 	}
-
-	if len(status.Modified) > 0 {
-		b.WriteString(fmt.Sprintf("%s%d", symbols.Modified, len(status.Modified)))
+	if status.Modified > 0 {
+		b.WriteString(fmt.Sprintf("%s%d", symbols.Modified, status.Modified))
 	}
-
-	if len(status.Untracked) > 0 {
-		b.WriteString(fmt.Sprintf("%s%d", symbols.Untracked, len(status.Untracked)))
+	if status.Untracked > 0 {
+		b.WriteString(fmt.Sprintf("%s%d", symbols.Untracked, status.Untracked))
 	}
-
-	if status.NumStashed > 0 {
-		b.WriteString(fmt.Sprintf("%s%d", symbols.Stashed, status.NumStashed))
-	}
-
-	if len(status.Staged)+len(status.Modified)+len(status.Untracked)+status.NumStashed == 0 {
-		b.WriteString(symbols.Clean)
+	if status.Stashed > 0 {
+		b.WriteString(fmt.Sprintf("%s%d", symbols.Stashed, status.Stashed))
 	}
 
 	b.WriteString(symbols.Suffix)
